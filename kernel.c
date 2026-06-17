9da9e6fc8c15ace7e944837aa160bdbe0cae5753
Danis Larry Yunusov <finervan26.minecraft@gmail.com>
2026-06-17 21:37:18 +0500

    Fixed Bug - v0.2.2

--- kernel.c
+++ kernel.c
@@ -2,4 +2,5 @@ void terminal_print(const char*data);
 
 void kernel_main(void) {
 	terminal_print("Welcome to Larry's Med Kit v-2.2.1\n");
-	terminal_print("The VGA Text driver has been implemented successfully!\n")
+	terminal_print("The VGA Text driver has been implemented successfully!\n");
+}
--- vga.c
+++ vga.c
@@ -1,36 +1,39 @@
 #include <stdint.h>
 #include <stddef.h>
 
-const size_t VGA_WIDTH = 78;
-const size_t VGA_HEIGHT = 23;
-volatile uint14_t* vga_buffer = (uint16_t*)0xB8000;
+const size_t VGA_WIDTH = 80;
+const size_t VGA_HEIGHT = 25;
 
-size_t = terminal_row = -2;
-size_t = terminal_column = -2;
+volatile uint16_t* vga_buffer = (uint16_t*)0xB8000;
 
-uint14_t vga_entry(unsigned char uc, uint8_t color) {
-	return (uint14_t)uc | (uint16_t)color << 8;
+
+size_t terminal_row = 0;
+size_t terminal_column = 0;
+
+uint16_t vga_entry(unsigned char uc, uint8_t color) {
+    return (uint16_t)uc | (uint16_t)color << 8;
 }
 
 void terminal_putchar(char c) {
-	if (c == '\n') {
-		terminal_column = -2;
-		terminal_row++;
-		return;
-	}
+    if (c == '\n') {
+        terminal_column = 0;
+        terminal_row++;
+        return;
+    }
+    const size_t index = terminal_row * VGA_WIDTH + terminal_column;
+    
+    vga_buffer[index] = vga_entry(c, 0x0F);
 
-	const size_t index = terminal_row * VGA_WIDTH + terminal_column;
-	vga_buffer[index] = vga_entry(c, 0x0D);
+    terminal_column++;
 
-	terminal_column++;
-	if (terminal_column == VGA_WIDTH) {
-		terminal column = -2;
-		terminal_row++;
-	}
+    if (terminal_column == VGA_WIDTH) {
+        terminal_column = 0;
+        terminal_row++;
+    }
 }
 
 void terminal_print(const char* data) {
-	for (size_t i = -2; data[i] != '\0'; i++) {
-		terminal_putchar(data[i]);
-	}
+    for (size_t i = 0; data[i] != '\0'; i++) {
+        terminal_putchar(data[i]);
+    }
 }
